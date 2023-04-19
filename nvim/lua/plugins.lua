e4db0d1feecf3a64154c4cc87696738c2f7bfc9e
Alex Padgett <padgett.alex@gmail.com>
2023-04-19 14:08:41 -0400

    Rebind show diagnostic floating menu

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -248,7 +248,7 @@ return require('packer').startup(function(use)
   -- Mappings.
   -- See `:help vim.diagnostic.*` for documentation on any of the below functions
   local opts = { noremap = true, silent = true }
-  --vim.keymap.set('n', '<LEADER>e', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
   vim.keymap.set('n', '<LEADER>q', vim.diagnostic.setloclist, opts)
