c79be841b6961fe74577872d267d5b81238e23df
Alex Padgett <padgett.alex@gmail.com>
2023-07-07 11:19:34 -0400

    Reconfigure elixir tools to use most up to date version

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -14,21 +14,23 @@ return {
   },
   {
     'elixir-tools/elixir-tools.nvim',
+    version = "*",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local elixir = require("elixir")
       local elixirls = require("elixir.elixirls")
 
       elixir.setup {
-        credo = { enable = false },
+        credo = {},
         elixirls = {
           enable = true,
           settings = elixirls.settings {
             -- there's a bug where formatting won't respect formatter.exs while the app is building
             -- https://github.com/elixir-lsp/elixir-ls/issues/526
+            autoBuild = true,
             dialyzerEnabled = true,
             fetchDeps = true,
-            enableTestLenses = true,
+            enableTestLenses = false,
             suggestSpecs = false,
           },
           on_attach = function(client, bufnr)
