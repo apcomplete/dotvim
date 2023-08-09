6d61dd85c672c828d3d2c8fab9619c58cc72f5ee
apcomplete <alex.padgett@workday.com>
2023-08-09 10:14:57 -0400

    Comment out elixir and hope that one day I get to use it again

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -12,38 +12,38 @@ return {
       end
     }
   },
-  {
-    'elixir-tools/elixir-tools.nvim',
-    version = "*",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local elixir = require("elixir")
-      local elixirls = require("elixir.elixirls")
+  --{
+    --'elixir-tools/elixir-tools.nvim',
+    --version = "*",
+    --dependencies = { "nvim-lua/plenary.nvim" },
+    --config = function()
+      --local elixir = require("elixir")
+      --local elixirls = require("elixir.elixirls")
 
-      elixir.setup {
-        credo = {},
-        elixirls = {
-          enable = true,
-          settings = elixirls.settings {
-            -- there's a bug where formatting won't respect formatter.exs while the app is building
-            -- https://github.com/elixir-lsp/elixir-ls/issues/526
-            autoBuild = true,
-            dialyzerEnabled = true,
-            fetchDeps = true,
-            enableTestLenses = false,
-            suggestSpecs = false,
-          },
-          on_attach = function(client, bufnr)
-            local bufopts = { buffer = bufnr, noremap = true }
-            require('util.on_attach')(client, bufnr)
-            -- remove the pipe operator
-            vim.keymap.set("n", "<leader>fp", ":ElixirFromPipe<cr>", bufopts)
-            -- add the pipe operator
-            vim.keymap.set("n", "<leader>tp", ":ElixirToPipe<cr>", bufopts)
-            vim.keymap.set("v", "<leader>em", ":ElixirExpandMacro<cr>", bufopts)
-          end
-        }
-      }
-    end
-  }
+      --elixir.setup {
+        --credo = {},
+        --elixirls = {
+          --enable = true,
+          --settings = elixirls.settings {
+            ---- there's a bug where formatting won't respect formatter.exs while the app is building
+            ---- https://github.com/elixir-lsp/elixir-ls/issues/526
+            --autoBuild = true,
+            --dialyzerEnabled = true,
+            --fetchDeps = true,
+            --enableTestLenses = false,
+            --suggestSpecs = false,
+          --},
+          --on_attach = function(client, bufnr)
+            --local bufopts = { buffer = bufnr, noremap = true }
+            --require('util.on_attach')(client, bufnr)
+            ---- remove the pipe operator
+            --vim.keymap.set("n", "<leader>fp", ":ElixirFromPipe<cr>", bufopts)
+            ---- add the pipe operator
+            --vim.keymap.set("n", "<leader>tp", ":ElixirToPipe<cr>", bufopts)
+            --vim.keymap.set("v", "<leader>em", ":ElixirExpandMacro<cr>", bufopts)
+          --end
+        --}
+      --}
+    --end
+  --}
 }
