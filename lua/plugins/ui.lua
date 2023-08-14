13683afe212eecc400438ef9ea0f2eb40fd31fc5
apcomplete <alex.padgett@workday.com>
2023-08-14 16:18:01 -0400

    Add trouble.nvim keybinding

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -49,11 +49,12 @@ return {
     'folke/trouble.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-      require('trouble').setup {
+      local trouble = require('trouble').setup {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
       }
+      vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
     end
   }
 }
